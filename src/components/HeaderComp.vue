a506c87d562bcc555718e41bb6a99190d20e4112
GioCri01 <giovanni.crispino94@icloud.com>
2022-05-13 17:18:44 +0200

    aggiunto componente header

---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,135 @@
+<template>
+    <header class="container">
+      <img src="../assets/img/dc-logo-bg.png" alt="">
+        <ul>
+            <li v-for="(item,index) in navBar " :key="`navBar-${index}`" :class="{'active': item.isActive}">
+                <a  :href="item.href">{{item.name}}</a>
+            </li>
+            
+        </ul>
+    </header>      
+              
+                  
+              
+              
+                  
+                
+            
+                
+              
+          
+      
+  
+</template>
+
+<script>
+export default {
+    name: "HeaderComp",
+    data(){
+        return{
+            navBar:[
+                {
+                    name: "characters",
+                    href: "#",
+                    isActive : false
+                   
+                },
+                {
+                    name: "comics",
+                    href: "#",
+                    isActive : false
+                   
+                },
+                {
+                    name: "movies",
+                    href: "#",
+                    isActive : true
+                   
+                },
+                {
+                    name: "tv",
+                    href: "#",
+                    isActive : false
+                   
+                },
+                {
+                    name: "games",
+                    href: "#",
+                    isActive : false
+                   
+                },
+                {
+                    name: "collectibles",
+                    href: "#",
+                    isActive : false
+                   
+                },
+                {
+                    name: "videos",
+                    href: "#",
+                    isActive : false
+                   
+                },
+                {
+                    name: "fans",
+                    href: "#",
+                    isActive : false
+                   
+                },
+                {
+                    name: "news",
+                    href: "#",
+                    isActive : false
+                   
+                },
+                {
+                    name: "shop",
+                    href: "#",
+                    isActive : false
+                   
+                },
+            ]
+        }
+       
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+header{
+    width: 100%;
+    height: 120px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    
+    
+}
+img{
+    
+    width: 80px;
+    
+}
+ul{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    list-style: none;
+    li{
+        margin: 20px;
+        text-transform: uppercase;
+        &.active{
+            border-bottom: 5px solid #1278f9 ;
+        }
+        a{
+            color: black;
+            text-decoration: none;
+            &:hover{
+                color: #1278f9;
+            }
+        }
+        
+    }
+}
+
+</style>
---
+++ src/components/MainComp.vue
@@ -0,0 +1,25 @@
+<template>
+  <main >
+      <div class="container">
+          asd
+      </div>
+      
+  </main>
+</template>
+
+<script>
+export default {
+    name:"MainComp"
+
+}
+</script>
+
+<style lang="scss" scoped>
+    main{
+        width: 100%;
+        height: 200px; //provvisorio
+        background-color: black;
+        color: white;
+    }
+
+</style>
